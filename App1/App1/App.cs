4e5aebed84de414ac750f8ffdb502f35082f2338
Goryudyuma <umaretekyoumade@gmail.com>
2016-04-01 11:39:44 +0900

    first commit
    やる気起きたらなんか作る

---
+++ App1/App1/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Start,
+                    Children = {
+                        new Label {
+                            //VerticalTextAlignment = TextAlignment.Start,
+                            //Text = (1+2).ToString(),
+                            HorizontalTextAlignment = TextAlignment.Start,
+                            Text = (1+2).ToString()
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
