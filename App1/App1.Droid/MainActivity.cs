44fbbdcea9221ce0b2045836b8e300883dfa5988
Goryudyuma <umaretekyoumade@gmail.com>
2016-04-02 11:49:37 +0900

    ちょっとバージョン情報がおかしい？ とりあえず今日はコミット。

--- App1/App1.Droid/MainActivity.cs
+++ App1/App1.Droid/MainActivity.cs
@@ -16,7 +16,8 @@ namespace App1.Droid
         {
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
--- App1/App1/App.cs
+++ App1/App1/App.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
 
 namespace App1
 {
@@ -11,7 +13,11 @@ namespace App1
     {
         public App()
         {
+            MainPage = new MapPage();
+
             // The root page of your application
+
+            /*
             MainPage = new ContentPage
             {
                 Content = new StackLayout
@@ -27,6 +33,7 @@ namespace App1
                     }
                 }
             };
+            */
         }
 
         protected override void OnStart()
@@ -42,6 +49,25 @@ namespace App1
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+    }
+
+    public class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                    new Position(35.71, 139.81),
+                    Distance.FromMiles(0.2)))
+            {
+                IsShowingUser = true,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            Content = new StackLayout
+            {
+                Children = { map }
+            };
         }
     }
 }
